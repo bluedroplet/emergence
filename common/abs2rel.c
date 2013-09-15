e4a2dc29bcc65b510d3f623ee97f3dbdc690755f
Jonathan Brown <jbrown@bluedroplet.com>
2013-09-15 22:37:09 +0200

    Add Copyright information.

--- common/abs2rel.c
+++ common/abs2rel.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * @copyright 1997 Shigio Yamaguchi
+ * @copyright 1997 Shigio Yamaguchi <shigio@tamacom.com>
  * @copyright 1999 Tama Communications Corporation
  * @license https://www.gnu.org/licenses/gpl-3.0.html
  * @homepage https://github.com/bluedroplet/emergence
